1bf18f205efc7f58ce066557826aaf3d82d25454
Chatt <canselstruck@gmail.com>
2024-02-10 19:42:09 +0100

    taking turns placing marks

--- pages/tictactoe.js
+++ pages/tictactoe.js
@@ -1,5 +1,22 @@
+var playersturn = "X";
+var marksPlaced = 0;
+
 function handleButton(number){
     var button = "button" + number + "TTT";
+    if(document.getElementById(button).innerText != "."  || marksPlaced == 9) return;
+    if(playersturn == "X"){
     document.getElementById(button).innerText = "X";
-    console.log("button",number,"TTT");
+    playersturn = "O";
+    } else {
+        document.getElementById(button).innerText = "O";
+        playersturn = "X";
+    }
+    marksPlaced++;
+    checkWinner();
+}
+
+function checkWinner(){
+    for(var i = 0; i <= 9; i = i +3){
+        var button = "button" + i + "TTT";
+    }
 }
